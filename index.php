7eb6fc5cd7ea4c81506aa62b214a974bf6cbbfc6
Kai <33795285+kaiicheng@users.noreply.github.com>
2023-07-07 15:12:14 -0400

    Add files via upload

---
+++ index.php
@@ -0,0 +1,15 @@
+<h1>Hello from Kai's first PHP Page!</h1>
+<p>
+<?php
+   echo "Hi there.\n";
+?>
+<p>
+    <?php
+   echo "What's the answer of 6 x 7?\n";
+    $answer = 6 * 7;
+   echo "The answer is $answer.\n";
+    ?>
+</p>
+
+</p>
+<p>Yes another paragraph.</p>
