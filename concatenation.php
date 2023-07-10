b721a7ca0fb4d2a5590f49fef20d0b6d9983072b
[your <tw841117@gmail.com>
2023-07-10 18:04:11 -0400

    Upload concatenation.php

---
+++ concatenation.php
@@ -0,0 +1,19 @@
+<?php
+    // First String
+    $a = 'Hello';
+    
+    // Second String
+    $b = 'World!';
+    
+    // Concatenation Of String
+    $c = $a.$b;
+    
+    echo " $a \n";
+    echo " <p> \n";
+    echo " $b \n";
+
+    echo " <p> \n";
+
+    // print Concatenate String
+    echo " $c \n";
+?>
