a38d0ce9c2204b54441b0a708321929c40a1f302
[your <tw841117@gmail.com>
2023-07-10 19:23:17 -0400

    comments.php

---
+++ comments.php
@@ -0,0 +1,11 @@
+<?php
+    echo " This is a test for comments.";  // first line of comment
+
+    echo " <p> \n";
+      
+    echo " This is another test for comments.";  /* second line of comment */
+
+    echo " <p> \n";
+
+    echo " This is the final test for comments.";  # third line of comment
+?>
