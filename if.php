e647418ed0b5b15aaa41581250789ebc5b388236
[your <tw841117@gmail.com>
2023-07-11 14:02:40 -0400

    upload if and hello_world

---
+++ if.php
@@ -0,0 +1,17 @@
+<?php
+    $ans = 42;
+
+    if ($ans == 42) {
+        print("Hello world!\n");
+    } else {
+        print("Wrong answer\n");
+    }
+?>
+
+<br>
+
+<!-- whitespace does not matter -->
+<?php
+    $ans = 42; if ($ans == 42) { print("Hello world!\n");
+    } else { print("Wrong answer\n"); }
+?>
