9ac56ac86c3ceb014a2dcd1e21e50c6e2b721bdb
[your <tw841117@gmail.com>
2023-07-11 12:58:33 -0400

    upload casting_true_false

---
+++ casting_true_false.php
@@ -0,0 +1,13 @@
+<?php
+    $a = 56; $b = 12;
+
+    $c = $a / $b;
+
+    // FALSE is "not there" - it is even "smaller" than zero, at least when it comes to width
+    echo "A" .FALSE. "B";
+
+    echo "<br>";
+
+    // TRUE becomes a string
+    echo "X" .TRUE. "Y";
+?>
