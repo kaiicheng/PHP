868bbdb1e505c3eced6c78472f8e823aa1e2ccc7
[your <tw841117@gmail.com>
2023-07-11 12:21:28 -0400

    .\side-effect.php

---
+++ side-effect.php
@@ -0,0 +1,17 @@
+<?php
+    $out = "Hello";
+
+    $out .= " world!";
+
+    $out .= "\n";
+    
+    echo $out;
+
+    echo "<br>";
+
+    $count = 0;
+
+    $count += 1;
+
+    echo "Count: $count\n";
+?>
