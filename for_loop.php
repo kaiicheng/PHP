979a04ceda1aba95a4a395c4684c6181f1010d3b
[your <tw841117@gmail.com>
2023-07-13 15:46:11 -0400

    upload for_loop

---
+++ for_loop.php
@@ -0,0 +1,15 @@
+<?php
+    for($count=1; $count<=6; $count++) {
+        echo "$count times 6 is " . $count * 6;
+        echo "<br>";
+    }
+?>
+
+<br>
+
+<?php
+    for($count=1; $count<=6; ++$count) {
+        echo "$count times 6 is " . $count * 6;
+        echo "<br>";
+    }
+?>
